c6efe3880d68187fc08b6c73b8a0304d543919f9
André Cini <andrevpcini@gmail.com>
2024-11-12 11:58:31 -0300

    [FEAT] - Criação de rota para retornar Imagem em base64

--- Domain/Caramel.Pattern.Services.Domain/Services/IBucketService.cs
+++ Domain/Caramel.Pattern.Services.Domain/Services/IBucketService.cs
@@ -5,5 +5,7 @@
         Task<string> UploadFileAsync(string base64Image, string key);
         Task<List<string>> GetImagesForEntityAsync(string key);
         Task DeleteFileAsync(string key);
+        Task<bool> ImageExistsAsync(string key);
+        Task<string> GetImageAsBase64Async(string key);
     }
 }
